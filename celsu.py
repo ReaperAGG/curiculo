d8c185e57d22215edf1a3658959bf64b27ed7d77
ReaperAGG <alencarantoniommf@gmail.com>
2022-06-15 10:48:20 -0300

    AAAA

--- celsu.py
+++ celsu.py
@@ -2,12 +2,7 @@ from tkinter import *
 
 root = Tk()
 
-def calcular():
-    if in1.get().replace('.','',1).isdigit():
-        c=float(in1.get())*1.8 + 32)
-        lb3['text'] = x
-    else:
-        lb3['text'] = 'Erro!!!'
+
 
 
 root.grid_rowconfigure(0, weight=1)
@@ -18,11 +13,11 @@ root.grid_columnconfigure(1, weight=1)
 
 
 
-lb1 = Label(root, text='Label 1', bg='green', fg='black', font='calibri 30')
-lb2 = Label(root, text='Label 1', bg='green', fg='black', font='calibri 30')
-lb3 = Label(root, text='Label 1', bg='green', fg='black', font='calibri 30')
-in1 = Entry (root, font= 'Arial 25')
-in2 = Entry (root, font= 'Arial 25')
+lb1 = Label(root, fg='black', font='calibri 30')
+lb2 = Label(root, fg='black', font='calibri 30')
+lb3 = Label(root,  fg='black', font='calibri 30')
+in1 = Entry (root, font= 'calibri 30')
+in2 = Entry (root, font= 'calibri 305')
 lb4 = Label(root, text='Label 1', bg='green', fg='black', font='calibri 30')
 lb5 = Button(root, text='Label 1', bg='green', fg='black', font='calibri 30')
 
